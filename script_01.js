15c15ce12bef60a0784f860876383091784bcd5e
MichaelErichMueller <Michael.Erich.Mueller@gmx.de>
2022-11-14 10:41:14 +0100

    Update

--- script_01.js
+++ script_01.js
@@ -11,11 +11,39 @@
 // output(add(2,3));
 // output(subtract(2,3))
 // output(multiply(2,4));
-// output(divide(10,5));
+// output(divide(2,2));
+// output(divide(2,0));
 
+
+// module: Calculator / Tests:
+// agreement: "+","-","*","/"
+output(calculator("+"));
+output(calculator("-"));
+output(calculator("*"));
+output(calculator("/"));
+output(calculator("#?!"));
+
+function calculator(op){
+switch (op) {
+    case "+": // add
+        return "add";
+    case "-": // subtract
+        return "sub";
+    case "*": // multiply
+        return "mul";
+    case "/": // divide 
+        return "divide";
+    default:    // Error
+        return "Error";
+    }
+}
 
 // Division
+// var 3.
 function divide(a,b) {
+    if (b==0){
+    }
+
     return a / b;
     }
 // Multiplikation

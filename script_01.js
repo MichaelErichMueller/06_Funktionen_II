441dfcecac40605989ac0aacf405a31239df1992
MichaelErichMueller <Michael.Erich.Mueller@gmx.de>
2022-11-14 09:21:30 +0100

    Update

--- script_01.js
+++ script_01.js
@@ -8,8 +8,28 @@
 4. Ausgabe in Konsole :
 */
 
+// output(add(2,3));
+// output(subtract(2,3))
+// output(multiply(2,4));
+// output(divide(10,5));
 
 
+// Division
+function divide(a,b) {
+    return a / b;
+    }
+// Multiplikation
+function multiply(a,b) {
+    return a * b;
+    }
+// Addition
+function add(a,b) {
+return a + b;
+}
+// Subtraktion
+function subtract(a,b) {
+    return a - b;
+    }
 // module: console output /Test
 function output(outputData){
 console.log(outputData);
